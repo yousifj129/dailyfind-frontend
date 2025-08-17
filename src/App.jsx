eca21c9dc25428e57a0b2866ca669a6f96343d4f
Yaqoob <yaqoob.buhejji@gmail.com>
2025-08-17 16:26:00 +0300

    Working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from 'react'
 import './App.css'
 import { getAllShoppingItems } from '../lib/api'
 import ShoppingItemsList from './components/ShoppingItemsList/ShoppingItemsList'
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import NavBar from "./components/NavBar/NavBar"
 
 function App() {
   const [shoppingItems, setShoppingItems] = useState(getAllShoppingItems())
   return (
-    <>
-      <ShoppingItemsList shoppingItems={shoppingItems}/>
-    </>
+    <Router>
+    
+      <Routes>
+        <Route path="/" element={<ShoppingItemsList shoppingItems={shoppingItems}/>} />
+        <Roue path = "" />
+
+
+      </Routes>
+
+
+
+    </Router>
+
   )
+
+
 }
 
 export default App
- 
+
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router"
+const NavBar = () =>{
+    return(
+        <>
+        <Link to ="/">HomePage</Link>
+        <Link to ="/ShoppingItems">Shopping Items</Link>
+        
+        </>
+    )
+}
+
+export default NavBar
