63ae704d39dfaff8031206e5a53b712f73c4ad19
yousifj129 <yousifjaffar129@gmail.com>
2025-08-17 15:31:45 +0300

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import { getAllShoppingItems } from '../lib/api'
 import ShoppingItemsList from './components/ShoppingItemsList/ShoppingItemsList'
+import ShoppingItemNewForm from './components/ShoppingItemNewForm/ShoppingItemNewForm'
 
 function App() {
   const [shoppingItems, setShoppingItems] = useState(getAllShoppingItems())
   return (
     <>
-      <ShoppingItemsList shoppingItems={shoppingItems}/>
+      <ShoppingItemNewForm/>
     </>
   )
 }
--- src/components/ShoppingItemsList/ShoppingItemsList.jsx
+++ src/components/ShoppingItemsList/ShoppingItemsList.jsx
@@ -3,9 +3,9 @@ import { Rating, ThinStar } from '@smastrom/react-rating'
 import '@smastrom/react-rating/style.css'
 import { Link } from "react-router"
 const myStyles = {
-  itemShapes: ThinStar,
-  activeFillColor: '#ffb700',
-  inactiveFillColor: '#fbf1a9'
+    itemShapes: ThinStar,
+    activeFillColor: '#ffb700',
+    inactiveFillColor: '#fbf1a9'
 }
 const ShoppingItemsList = ({ shoppingItems }) => {
     return (
@@ -18,20 +18,20 @@ const ShoppingItemsList = ({ shoppingItems }) => {
                     ) / shoppingItem.reviews.length
 
                     return (
-                        
-                        <Link to={`/shoppingitems/${shoppingItem._id}`}>
-                       
-                        <div key={index} className="shoppingItem">
+
+                        // <Link to={`/shoppingitems/${shoppingItem._id}`} >
+
+                        <div className="shoppingItem" key={index}>
 
                             <img src={shoppingItem.images[0]} alt={`${shoppingItem.itemName} image`} className="itemImage" />
                             <h3>{shoppingItem.itemName}</h3>
-                            <p style={{ display: "flex" }}>
-                                <Rating style={{ maxWidth: 90 }} value={rating} readOnly={true} itemStyles={myStyles} />
+                            <Rating style={{ maxWidth: 90, display:"flex" }} value={rating} readOnly={true} itemStyles={myStyles}>
                                 <span style={{ justifySelf: "flex-end", alignSelf: "center" }}>{rating}/5</span>
-                            </p>
+                            </Rating>
+
                             <p><strong>{shoppingItem.price}$</strong> </p>
                         </div>
-                        </Link>
+                        // </Link>
                     )
                 })}
             </div>
